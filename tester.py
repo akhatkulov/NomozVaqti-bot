79424334e74bf9c0bdf3a21bf953fb4439f28c38
akhatkulov <amsmetafra@gmail.com>
2024-01-03 16:58:49 +0500

    fixxing 3

--- tester.py
+++ tester.py
@@ -1,7 +1,23 @@
-t = ''
+import requests
+from bs4 import BeautifulSoup
 
-for i in range(0,100):
-    t=t+"btn"+str(i)+','
-    print(t)
+def pray_time(city):
+    try:
+        url = f"https://namozvaqti.uz/shahar/{city}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        bomdod = soup.find("p", {"id": "bomdod"}).text.strip()
+        quyosh = soup.find("p", {"id": "quyosh"}).text.strip()
+        peshin = soup.find("p", {"id": "peshin"}).text.strip()
+        asr = soup.find("p", {"id": "asr"}).text.strip()
+        shom = soup.find("p", {"id": "shom"}).text.strip()
+        xufton = soup.find("p", {"id": "hufton"}).text.strip()
 
-print(t)
+        message = f"🕌{city} namoz vaqtlari\n🌙Bomdod: {bomdod}\n🌝Quyosh: {quyosh}\n🌇Peshin: {peshin}\n🌅Asr: {asr}\n🌄Shom: {shom}\n🌘Xufton: {xufton}"
+        return message
+    except:
+        return "mavjud emas"
+
+while True:
+    a = input("Shaharni kiriting: ")
+    print(pray_time(a))
